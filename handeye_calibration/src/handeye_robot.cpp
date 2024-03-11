629cb190bbfff8c6889faeb02e17632969e71497
malasiot <malasiot@gmail.com>
2024-03-11 19:32:32 +0200

    aruco

--- handeye_calibration/src/handeye_robot.cpp
+++ handeye_calibration/src/handeye_robot.cpp
@@ -1,9 +1,13 @@
 #include "handeye_robot.hpp"
 #include "robot_commander.hpp"
 #include "util.hpp"
-
+#include <opencv2/highgui.hpp>
 #include <geometry_msgs/msg/pose.hpp>
 #include <random>
+
+#include <opencv2/aruco.hpp>
+#include <opencv2/objdetect/objdetect.hpp>
+
 using namespace std ;
 using namespace Eigen ;
 
@@ -41,16 +45,66 @@ std::vector<Affine3d> compute_poses_around_current_state(const Affine3d &pose, s
     return samples ;
 
 }
-HandEyeRobotCommander::HandEyeRobotCommander(): rclcpp::Node("handeye_robot", rclcpp::NodeOptions().automatically_declare_parameters_from_overrides(true)) {
+HandEyeRobotCommander::HandEyeRobotCommander():rclcpp::Node("handeye_robot", rclcpp::NodeOptions().automatically_declare_parameters_from_overrides(true)) {
 
 }
 
+void HandEyeRobotCommander::imageCallback(const sensor_msgs::msg::Image::ConstSharedPtr& msg) {
+    cv_bridge::CvImageConstPtr cv_ptr = cv_bridge::toCvShare(msg, sensor_msgs::image_encodings::TYPE_8UC3);
+    image_ = cv_ptr->image ;
+}
 
+void HandEyeRobotCommander::setup()
+{
+
+    image_transport_ = std::make_shared<image_transport::ImageTransport>(shared_from_this());
+
+    image_sub_ = std::make_shared<image_transport::Subscriber>(image_transport_->subscribe("/image", 10, std::bind(&HandEyeRobotCommander::imageCallback, this, std::placeholders::_1)));
+    RCLCPP_INFO(get_logger(), "After subscribing");
+
+}
+
+void HandEyeRobotCommander::moveRobot()
+{
+
+}
+
+void HandEyeRobotCommander::showImage() {
+    try {
+
+        std::vector<int> markerIds;
+        std::vector<std::vector<cv::Point2f>> markerCorners, rejectedCandidates;
+        cv::aruco::DetectorParameters detectorParams = cv::aruco::DetectorParameters();
+        cv::Ptr<cv::aruco::Dictionary> dictionary(cv::aruco::getPredefinedDictionary(cv::aruco::DICT_4X4_50));
+
+        cv::aruco::detectMarkers(image_, dictionary, markerCorners, markerIds, cv::aruco::DetectorParameters::create(), rejectedCandidates);
+        cv::imshow("view", image_);
+
+        if(markerIds.size() > 0) {
+            cv::Ptr<cv::aruco::Board> board = cv::aruco::Board::create(markerCorners, dictionary, markerIds);
+
+            cv::Vec3d rvec, tvec;
+            int valid = cv::aruco::estimatePoseBoard(markerCorners, markerIds, board, cv::Mat(), cv::Mat(), rvec, tvec);
+
+        }
+
+        } catch (cv_bridge::Exception &e) {
+          auto logger = rclcpp::get_logger("my_subscriber");
+     //     RCLCPP_ERROR(logger, "Could not convert from '%s' to 'bgr8'.",
+     //                  msg->encoding.c_str());
+        }
+}
+
+void HandEyeRobotCommander::moveRobotCallback(const std::shared_ptr<handeye_calibration_msgs::srv::MoveRobot::Request> request, const std::shared_ptr<handeye_calibration_msgs::srv::MoveRobot::Response> response)
+{
+    std::thread(&HandEyeRobotCommander::moveRobot, this).detach() ;
+}
 
 int main(int argc, char *argv[]) {
     rclcpp::init(argc, argv);
 
     auto server = std::make_shared<HandEyeRobotCommander>();
+    server->setup() ;
 
     rclcpp::executors::SingleThreadedExecutor executor;
     executor.add_node(server);
@@ -62,39 +116,42 @@ int main(int argc, char *argv[]) {
     auto ee_pose = commander->getCurrentPose("r_tool0") ;
     Affine3d ep = poseMsgToEigenAffine(ee_pose.pose);
     auto poses = compute_poses_around_current_state(ep, 100) ;
+
     uint current_pose = 0 ;
     while ( 1 ) {
         cout << "Press <enter> to move to a new position or <q> to quit" << endl ;
         char c = getchar();
         if ( c == '\n') {
-           auto pose = poses[current_pose] ;
+            auto pose = poses[current_pose] ;
 
-           geometry_msgs::msg::Pose target_pose ;
+            geometry_msgs::msg::Pose target_pose ;
 
-           poseEigenAffineToMsg(pose, target_pose) ;
+            poseEigenAffineToMsg(pose, target_pose) ;
 
-           commander->setPoseTarget(target_pose);
+            commander->setPoseTarget(target_pose);
 
-           moveit::planning_interface::MoveGroupInterface::Plan plan;
-           commander->setEndEffectorLink("r_tool0") ;
+            moveit::planning_interface::MoveGroupInterface::Plan plan;
+            commander->setEndEffectorLink("r_tool0") ;
 
-           commander->setStartStateToCurrentState();
-           auto const ok = static_cast<bool>(commander->plan(plan));
+            commander->setStartStateToCurrentState();
+            auto const ok = static_cast<bool>(commander->plan(plan));
 
-           // Execute the plan
-           if(ok) {
-             commander->execute(plan);
-           } else {
-             RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Planning failed!");
-           }
+            // Execute the plan
+            if(ok) {
+                commander->execute(plan);
+            } else {
+                RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Planning failed!");
+            }
 
-           current_pose ++ ;
+            server->showImage() ;
+            current_pose ++ ;
 
 
         } else if ( c == 'q' ) break ;
     }
 
-    rclcpp::shutdown();
     spinner.join() ;
+    rclcpp::shutdown();
+
 }
 
--- handeye_calibration/src/handeye_robot.hpp
+++ handeye_calibration/src/handeye_robot.hpp
@@ -10,17 +10,24 @@
 #include "tf2_ros/buffer.h"
 #include "robot_commander.hpp"
 #include "handeye_calibration_msgs/srv/move_robot.hpp"
+#include "image_transport/image_transport.hpp"
+#include "cv_bridge/cv_bridge.h"
+
 
 
 class HandEyeRobotCommander:  public rclcpp::Node {
 public:
     HandEyeRobotCommander();
 
-    void setup(const std::shared_ptr<RobotCommander> &c);
+    void setup();
+
+    void moveRobot() ;
+
+    void showImage();
 private:
     void moveRobotCallback(const std::shared_ptr<handeye_calibration_msgs::srv::MoveRobot::Request> request,
                            const std::shared_ptr<handeye_calibration_msgs::srv::MoveRobot::Response> response) ;
-
+ void imageCallback(const sensor_msgs::msg::Image::ConstSharedPtr &msg);
     void jointStateCallback(const sensor_msgs::msg::JointState::SharedPtr state);
     void updateTransforms(const std::map<std::string, double> &joint_positions, const builtin_interfaces::msg::Time & time);
     void fetchCameraFrame() ;
@@ -37,4 +44,10 @@ private:
     std::string target_frame_ ;
     std::shared_ptr<tf2_ros::TransformListener> tf_listener_{nullptr};
     std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
+    std::vector<Eigen::Affine3d> poses_ ;
+    uint current_pose_ = 0;
+
+    std::shared_ptr<image_transport::ImageTransport> image_transport_;
+    std::shared_ptr<image_transport::Subscriber> image_sub_;
+    cv::Mat image_ ;
 };
